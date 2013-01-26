59cb2a66268bbda2ef4cc1fb3ac82b9dbdd74ad4
Sin Shimozono <una.veritas@me.com>
2013-01-26 20:41:03 +0900

    glcd

--- libraries/glcd/config/ks0108_Arduino.h
+++ libraries/glcd/config/ks0108_Arduino.h
@@ -20,21 +20,21 @@
 /*********************************************************/
 /* Data pin definitions
  */
-#define glcdData0Pin        4
-#define glcdData1Pin        5
-#define glcdData2Pin        6
-#define glcdData3Pin        7
-#define glcdData4Pin        8
-#define glcdData5Pin        9
-#define glcdData6Pin        10
-#define glcdData7Pin        11
+#define glcdData0Pin        8
+#define glcdData1Pin        9
+#define glcdData2Pin        10
+#define glcdData3Pin        11
+#define glcdData4Pin        4
+#define glcdData5Pin        5
+#define glcdData6Pin        6
+#define glcdData7Pin        7
 
 /* Arduino pins used for Commands
  * default assignment uses the first five analog pins
  */
 
-#define glcdCSEL1        12
-#define glcdCSEL2        13
+#define glcdCSEL1        14
+#define glcdCSEL2        15
 
 #if NBR_CHIP_SELECT_PINS > 2
 #define glcdCSEL3         3   // third chip select if needed
@@ -44,10 +44,10 @@
 #define glcdCSEL4         2   // fourth chip select if needed
 #endif
 
-#define glcdRW           15
-#define glcdDI           14
-#define glcdEN           16
+#define glcdRW           16
+#define glcdDI           17
+#define glcdEN           18
 // Reset Bit  - uncomment the next line if reset is connected to an output pin
-#define glcdRES          17    // Reset Bit
+//#define glcdRES          19    // Reset Bit
 
 #endif //GLCD_PIN_CONFIG_H
